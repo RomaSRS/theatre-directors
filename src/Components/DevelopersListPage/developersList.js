1473e0b635660577d86faf0ab6a9e75d8c938e0d
Anton Yasko <yasko.anton.92@gmail.com>
2020-02-22 21:07:15 +0300

    fix: correct language switching

--- src/Components/DevelopersListPage/developersList.js
+++ src/Components/DevelopersListPage/developersList.js
@@ -1,20 +1,18 @@
 import React, { PureComponent } from 'react';
-import {default as configEN} from '../../data/configEN.js';
 import Developer from './developer.js';
 import './developersList.css';
 
 export default class DevelopersList extends PureComponent {
   render() {
-    // const {
-    //   lang,
-    // } = this.props;
+    const {
+      developers
+    } = this.props;
 
     return (
       <div className="developers-list">
-        {configEN.developers.map((developerData) => {
+        {developers.map(developerData => {
           return (
             <Developer
-              // lang={lang}
               developerData={developerData}
             />
           );
