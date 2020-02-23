219f17baede9cdbeec4aa8381cbda47302ef1acf
romasrs <shaman.roman@tut.by>
2020-02-23 13:29:02 +0300

    init: updated content

--- src/Components/mainPage/main.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import configEN from "../../data/configEN";
-import configBY from "../../data/configBY";
-import configRU from "../../data/configRU";
-import SelectLang from "./select/select";
