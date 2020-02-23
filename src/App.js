ec87ec6edd4de7450094c09c84543591bb19bcbc
Katsiaryna31 <katia310790@mail.ru>
2020-02-23 13:51:51 +0300

    feat:add main page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import DirectorNavigation from "./Components/DirectorListPage/DirectorNavigation";
 import Worklog from "./Components/WorklogPage/Worklog";
 import DevelopersList from "./Components/DevelopersListPage/DevelopersList";
+import MainPage from "./Components/MainPage/Main";
 import configEN from "./Data/ConfigEN";
 import configBY from "./Data/ConfigBY";
 import configRU from "./Data/ConfigRU";
@@ -109,7 +110,7 @@ class App extends React.Component {
 					<div className="main">
 						<Switch>
 							<Route exact path="/">
-								<div>Desktop 1</div>
+								<MainPage data={data}/>
 							</Route>
 							<Route path="/list">
 								<DirectorNavigation author={data.directors} />
