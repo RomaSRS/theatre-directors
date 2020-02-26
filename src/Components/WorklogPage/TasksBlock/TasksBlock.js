3c9655495f80858b9482cc685cbd6ac20d58428d
Kirill Leogky <kirillmargheriti@gmail.com>
2020-02-26 23:02:29 +0300

    feat: add tasks that we are made

---
+++ src/Components/WorklogPage/TasksBlock/TasksBlock.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Checkbox from '@material-ui/core/Checkbox';
+import Box from '@material-ui/core/Box';
+import './TasksBlock.css';
+
+
+export default function TasksBlock(props) {
+  return (
+    <Box className="self_evaluation_block">
+      <h3 className="self_evaluation_block-title">{props.task.tasksTitle}</h3>
+      {props.task.tasks.map((elem) =>
+        <div className="self_evaluation_block-task" key={elem}>
+          <Checkbox defaultChecked />
+          <span  className="self_evaluation_block-task-about">
+            {elem}
+          </span>
+        </div>
+      )}
+    </Box>
+  )
+}
--- src/Data/ConfigEN.js
+++ src/Data/ConfigEN.js
@@ -26,6 +26,27 @@ const styleguideNav = {
   letterSpacing: 'Letter Spacing',
 };
 
+const tasksData = {
+  tasksTitle: 'Maximum points - 240',
+  tasks: [
+    'Main page + page with a list of authors + author’s page - 10',
+    'Page with team members + page with worklog - 10',
+    'Page with list of authors contains search widget - 10',
+    'Portal has two languages - 20',
+    'Portal has page with styleguide - 20',
+    'Mobile version is okey - 10',
+    'Ipad/tablet version is okey - 10',
+    'Author’s page contains timeline - 10',
+    'Author’s page contains video overlay - 10',
+    'Author’s page contains photo gallery - 20',
+    'Author’s page contains map - 10',
+    'Design - 20',
+    'Material-ui is used - 20',
+    'Portal has third language - 10',
+    'Animations - 20',
+  ],
+};
+
 const developers = [
   {
     id: 'D0',
@@ -489,6 +510,7 @@ const secondQuote = {
 const directorOfTheDay = 'Director of the day';
 
 export default {
+  tasksData,
   styleguideNav,
   colors,
   developers,
